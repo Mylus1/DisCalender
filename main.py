79896226eac4705a949e2f85676a4ac0e6241d17
Bilal Leadbitter <bilal.leadbitter@gmail.com>
2021-10-01 21:22:37 +0100

    SQLite database recording dates and times

--- main.py
+++ main.py
@@ -7,12 +7,23 @@ import sqlite3
 import db
 
 con = sqlite3.connect("reminders.db")
-cur = con.cursor()
 bot = commands.Bot(command_prefix='!')
 load_dotenv()
 currentTime = datetime.datetime.now()
 formatDate = currentTime.strftime("%x")
 
+con.execute("""CREATE TABLE IF NOT EXISTS reminders (
+    date TEXT,
+    time TEXT
+)""")
+
+@bot.command(name="SetReminder")
+async def set_reminder(ctx, date, time):
+    cur = con.cursor()
+    reminder = await ctx.send(f"Set for {date} at {time}")
+    cur.execute("INSERT INTO reminders VALUES (?, ?)", (date, time))
+    con.commit()
+    
 def get_local_date():
     currentDate = datetime.datetime.now()
     localDate = currentDate.strftime("%x")
@@ -23,10 +34,6 @@ def get_local_time():
     currentTime = datetime.datetime.now()
     localTime = currentTime.strftime("%X")
     return localTime
-
-@bot.command(name="SetReminder")
-async def set_reminder(ctx, date, time):
-    await ctx.send(f"Set for {date} at {time}")
 
 @bot.command(name="CurrentDate")
 async def current_date(ctx):
